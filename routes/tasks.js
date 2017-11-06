2b28065b4921b53bcda018422b93329a719df16f
Olanrewaju Ebenezer <olanrewaju.ebenezer@VentureGardenGroup.Local>
2017-11-06 07:05:08 +0100

    Added task post

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var dbconnUrl = 'mongodb://tobi:adetoberu@ds149855.mlab.com:49855/liferon_tasks';
-//var dbconnUrl = 'mongodb://tobi:adetoberu@localhost:27017/mytasklist';
+//var dbconnUrl = 'mongodb://tobi:adetoberu@ds149855.mlab.com:49855/liferon_tasks';
+var dbconnUrl = 'mongodb://tobi:adetoberu@localhost:27017/mytasklist';
 var collectionList = ['tasks'];
 var db = mongojs(dbconnUrl, collectionList);
 
-router.get('/tasks', function(req, res, next) {
+// Get all tasks
+router.get('/task', function(req, res, next) {
   db.tasks.find(function(err, tasks) {
     if (err) {
       res.send(err);
@@ -15,4 +16,32 @@ router.get('/tasks', function(req, res, next) {
   })
 });
 
+// Get Single task
+router.get('/task/:id', function(req, res, next) {
+  db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(task);
+  })
+});
+
+// Save task
+router.post('/task', function(req, res, next) {
+  var task = req.body;
+  console.log(task);
+  if (!task.title || task.isDone) {
+    res.status(400);
+    res.json({
+      "error": "Bad data"
+    });
+  } else {
+    db.tasks.save(task, function (err, task) {
+      if (err) {
+        res.end(err);
+      }
+      res.json(task);
+    });
+  }
+});
 module.exports = router;
