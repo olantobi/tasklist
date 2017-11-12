16695c2a644ac5e3971107218c98f529196b9a76
Olanrewaju Ebenezer <olanrewaju.ebenezer@VentureGardenGroup.Local>
2017-11-12 06:58:23 +0100

    Added working versions of angular2 libraries

--- routes/tasks.js
+++ routes/tasks.js
@@ -44,4 +44,42 @@ router.post('/task', function(req, res, next) {
     });
   }
 });
+
+// Delete task
+router.delete('/task/:id', function(req, res, next) {
+  db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err) {
+    if (err) {
+      res.send(err);
+    }
+    res.status(200);
+  })
+});
+
+// Update task
+router.put('/task/:id', function(req, res, next) {
+  var task = req.body;
+  var updatedTask = {};
+
+  if (task.isDone) {
+    updatedTask.isDone = task.isDone;
+  }
+
+  if (task.title) {
+    updatedTask.title = task.title;
+  }
+
+  if (!updatedTask) {
+    res.status(400);
+    res.json({
+      "error": "Bad data"
+    })
+  } else {
+    db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, updatedTask, {}, function(err, task) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(task);   
+    })
+  }
+});
 module.exports = router;
